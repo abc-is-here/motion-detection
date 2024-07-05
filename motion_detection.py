500f368b0cbdcb8a4e7f52fda6293aa3d505b9e1
abc <abhaygupta.hsj@gmail.com>
2024-07-05 16:50:08 +0530

    The code has been improved!

--- motion_detection.py
+++ motion_detection.py
@@ -1,5 +1,6 @@
 import cv2
 
+min_area = 500
 video_source = 0
 
 cap = cv2.VideoCapture(video_source)
@@ -9,17 +10,30 @@ if not cap.isOpened():
 
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
     fgmask = fgbg.apply(frame)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    cv2.imshow('Original Frame', frame)
-    cv2.imshow('Foreground Mask', fgmask)
+    for contour in contours:
+        if cv2.contourArea(contour) < min_area:
+            continue
+
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Motion Detection', frame)
     
     if cv2.waitKey(30) & 0xFF == 27:
         break
 
+cap.release()
 cv2.destroyAllWindows()
